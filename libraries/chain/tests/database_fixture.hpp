7a0608b67f03927e3dcd071400e89af138352022
Nathan Hourt <nat.hourt@gmail.com>
2015-02-27 17:15:02 -0500

    Insubstantial tweaks

--- libraries/chain/tests/database_fixture.hpp
+++ libraries/chain/tests/database_fixture.hpp
@@ -15,22 +15,21 @@ struct database_fixture {
    database_fixture()
    {
       db.init_genesis();
-      BOOST_REQUIRE(key_id_type()(db));
-      genesis_key = key_id_type()(db)->id;
+      BOOST_REQUIRE(genesis_key(db));
       db.push_undo_state();
    }
    ~database_fixture(){}
 
    account_create_operation make_account() {
       account_create_operation create_account;
-//      create_account.name = "nathan";
+      create_account.fee_paying_account = db.get_account_index().get("init0")->id;
+      create_account.fee = asset();
 
-//      create_account.paying_account = db.get_account_index().get("init0")->id;
+//      create_account.name = "nathan";
 //      create_account.owner.add_authority(genesis_key, 123);
 //      create_account.active.add_authority(genesis_key, 321);
 //      create_account.memo_key = genesis_key;
 //      create_account.voting_key = genesis_key;
-//      create_account.registration_fee = asset();
 
       return create_account;
    }
