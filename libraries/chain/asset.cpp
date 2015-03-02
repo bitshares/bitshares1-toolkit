9894838461cc3768a97d855e9cf6e477120de9d1
Daniel Larimer <dan@bitshares.org>
2015-03-02 09:58:37 -0500

    remove rounding and division from price < operator

--- libraries/chain/asset.cpp
+++ libraries/chain/asset.cpp
@@ -9,15 +9,9 @@ namespace bts { namespace chain {
          if( a.base.asset_id > b.base.asset_id ) return false;
          if( a.quote.asset_id < b.quote.asset_id ) return true;
          if( a.quote.asset_id > b.quote.asset_id ) return false;
-
-         FC_ASSERT( a.base.amount.value > 0 );
-         FC_ASSERT( b.base.amount.value > 0 );
-         auto amult = (fc::uint128(BTS_MAX_SHARE_SUPPLY) * a.quote.amount.value)/a.base.amount.value;
-         auto bmult = (fc::uint128(BTS_MAX_SHARE_SUPPLY) * b.quote.amount.value)/b.base.amount.value;
+         auto amult = fc::uint128(a.quote.amount.value) * b.base.amount.value; 
+         auto bmult = fc::uint128(b.quote.amount.value) * a.base.amount.value; 
          return amult < bmult;
-         //return boost::rational<uint64_t>(a.quote.amount.value,a.base.amount.value) 
-         //       <
-         //       boost::rational<uint64_t>(b.quote.amount.value,b.base.amount.value);
       }
       asset operator * ( const asset& a, const price& b )
       {
