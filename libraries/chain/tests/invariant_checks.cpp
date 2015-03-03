c8e5ee5f0d8bf3f5683f18d0849d00b3410b6fa3
Nathan Hourt <nat.hourt@gmail.com>
2015-03-03 10:58:58 -0500

    Finish initial version of balances invariant check

--- libraries/chain/tests/invariant_checks.cpp
+++ libraries/chain/tests/invariant_checks.cpp
@@ -14,20 +14,17 @@ BOOST_FIXTURE_TEST_SUITE( invariant_checks, database_fixture )
 
 BOOST_AUTO_TEST_CASE( share_supply )
 {
-   wdump( (sizeof(operation) ) );
-
    auto current_supply = db.get_base_asset()->dynamic_asset_data_id(db)->current_supply;
    BOOST_CHECK(db.get_base_asset()->max_supply == current_supply);
 
    simple_index<account_balance_object>& balance_index = dynamic_cast<simple_index<account_balance_object>&>(db.get_index<account_balance_object>());
-   //What do I do now?
-   BOOST_CHECK(false);
-   for( const auto& a : db.get_account_index() )
+   share_type total_balances = 0;
+   for( const account_balance_object* a : balance_index )
    {
+      total_balances += a->get_balance(asset_id_type()).amount;
    }
-   for( const auto& a : balance_index )
-   {
-   }
+
+   BOOST_CHECK( total_balances == BTS_INITIAL_SUPPLY );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
