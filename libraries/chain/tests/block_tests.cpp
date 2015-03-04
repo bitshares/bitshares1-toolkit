ee9c339a5f5824a9f161020a46e4481aaac85b7e
Daniel Larimer <dlarimer@invictus-innovations.com>
2015-03-04 18:23:57 -0500

    testing block production

---
+++ libraries/chain/tests/block_tests.cpp
@@ -0,0 +1,36 @@
+#include <bts/chain/database.hpp>
+#include <bts/chain/operations.hpp>
+#include <bts/chain/account_index.hpp>
+#include <bts/chain/asset_index.hpp>
+#include <bts/chain/time.hpp>
+#include <bts/chain/key_object.hpp>
+
+#include <fc/crypto/digest.hpp>
+
+#include "database_fixture.hpp"
+
+using namespace bts::chain;
+
+BOOST_FIXTURE_TEST_SUITE( operation_unit_tests, database_fixture )
+
+BOOST_AUTO_TEST_CASE( generate_empty_blocks )
+{
+   try {
+      fc::temp_directory data_dir;
+      db.open(data_dir.path(), genesis_allocation() );
+
+      start_simulated_time( bts::chain::now() );
+
+      auto delegate_priv_key  = fc::ecc::private_key::regenerate(fc::sha256::hash(string("genesis")) );
+      for( uint32_t i = 0; i < 100; ++i )
+      {
+         auto ad = db.get_global_properties()->active_delegates;
+         advance_simulated_time_to( db.get_next_generation_time(  ad[i%ad.size()] ) );
+         db.generate_block( delegate_priv_key, ad[i%ad.size()] );
+      }
+   } catch (fc::exception& e) {
+      edump((e.to_detail_string()));
+      throw;
+   }
+}
+BOOST_AUTO_TEST_SUITE_END()
