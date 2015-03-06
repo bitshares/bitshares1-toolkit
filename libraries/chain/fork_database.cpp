0925ea5a2274c831111b78c41d19fbb91253eaef
Daniel Larimer <dan@bitshares.org>
2015-03-06 10:48:33 -0500

    refactor fork database to use multiindex

--- libraries/chain/fork_database.cpp
+++ libraries/chain/fork_database.cpp
@@ -1,17 +1,14 @@
 #include <bts/chain/fork_database.hpp>
 
-namespace bts { namespace chain {
 
+namespace bts { namespace chain {
 fork_database::fork_database()
-:_by_num(1024)
 {
 }
 void fork_database::reset()
 {
    _head.reset();
-   _by_num.resize(0);
-   _by_num.resize(1024);
-   _by_id.clear();
+   _index.clear();
 }
 
 void fork_database::pop_block()
@@ -21,57 +18,46 @@ void fork_database::pop_block()
 
 void     fork_database::start_block( signed_block b )
 {
-   auto item = std::make_shared<fork_item>();
-   item->data = std::move(b);
-   auto id = item->data.id();
-   _by_num.resize(1024);
-   _by_num[item->data.block_num()%1024].push_back(item);
-   _by_id[id] = item;
+   auto item = std::make_shared<fork_item>( std::move(b) );
+   _index.insert( item );
    _head = item;
 }
 
 shared_ptr<fork_item>  fork_database::push_block( signed_block b )
 {
-   auto item = std::make_shared<fork_item>();
-   item->data = std::move(b);
-   auto id = item->data.id();
-   auto itr = _by_id.find( id );
-   FC_ASSERT( itr == _by_id.end() );
-   auto prev_itr = _by_id.find( item->data.previous );
-   if( item->data.block_num() > 1 )
+   auto item = std::make_shared<fork_item>( std::move(b) );
+
+   if( item->num > 1 )
    {
-      FC_ASSERT( prev_itr != _by_id.end(), "unable to find prior", ("prior",item->data.previous)("num",item->data.block_num()) );
-      item->prev = prev_itr->second;
-      FC_ASSERT( prev_itr->second->data.block_num() == item->data.block_num()-1 );
-   }
-   _by_id[id] = item;
-
-   auto n = item->data.block_num() % 1024;
-   if( _by_num[n].size() && _by_num[n][0]->data.block_num() != item->data.block_num() )
-   {
-      for( auto i : _by_num[n] )
-         _by_id.erase( i->data.id() );
-      _by_num[n].resize(0);
-   }
-
-   _by_num[n].push_back(item);
-
-   if( !_head || item->data.block_num() > _head->data.block_num() )
+      auto itr = _index.get<block_id>().find( b.previous );
+      FC_ASSERT( itr != _index.get<block_id>().end() );
+      FC_ASSERT( !(*itr)->invalid );
+      item->prev = *itr;
       _head = item;
+   }
+   else _head = item;
+
+   _index.insert( item );
+   if( item->num > _head->num )
+   {
+      _head = item;
+      _index.get<block_num>().erase( _head->num - 1024 );
+   }
    return _head;
 }
+
 pair<fork_database::branch_type,fork_database::branch_type>  
   fork_database::fetch_branch_from( block_id_type first, block_id_type second )const
 {
-   auto first_branch_itr = _by_id.find(first);
-   FC_ASSERT( first_branch_itr != _by_id.end() );
-   auto first_branch = first_branch_itr->second;
-
-   auto second_branch_itr = _by_id.find(second);
-   FC_ASSERT( second_branch_itr != _by_id.end() );
-   auto second_branch = second_branch_itr->second;
-
    pair<branch_type,branch_type> result;
+   auto first_branch_itr = _index.get<block_id>().find(first);
+   FC_ASSERT( first_branch_itr != _index.get<block_id>().end() );
+   auto first_branch = *first_branch_itr;
+
+   auto second_branch_itr = _index.get<block_id>().find(second);
+   FC_ASSERT( second_branch_itr != _index.get<block_id>().end() );
+   auto second_branch = *second_branch_itr;
+
 
    while( first_branch->data.block_num() > second_branch->data.block_num() )
    {
@@ -99,10 +85,7 @@ void fork_database::set_head( shared_ptr<fork_item> h )
 
 void fork_database::remove( block_id_type id )
 {
-   auto itr = _by_id.find(id);
-   if( itr == _by_id.end() ) return;
-   if( itr->second == _head ) _head.reset();
-   _by_id.erase(id);
+   _index.get<block_id>().erase(id);
 }
 
 } } // bts::chain
--- libraries/chain/include/bts/chain/fork_database.hpp
+++ libraries/chain/include/bts/chain/fork_database.hpp
@@ -2,11 +2,28 @@
 #include <bts/chain/block.hpp>
 #include <bts/chain/types.hpp>
 
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+
 namespace bts { namespace chain {
+   using boost::multi_index_container;
+   using namespace boost::multi_index;
 
    struct fork_item 
    {
+      fork_item( signed_block d )
+      :num(d.block_num()),id(d.id()),data( std::move(d) ){}
+
       weak_ptr< fork_item > prev;
+      uint32_t              num;
+      /**
+       * Used to flag a block as invalid and prevent other blocks from
+       * building on top of it.
+       */
+      bool                  invalid = false;
+      block_id_type         id;
       signed_block          data;
    };
 
@@ -43,9 +60,19 @@ namespace bts { namespace chain {
           */
          pair< branch_type, branch_type >  fetch_branch_from( block_id_type first, 
                                                               block_id_type second )const;
+
+         struct block_id{};
+         struct block_num{};
+         typedef multi_index_container< 
+            item_ptr,
+            indexed_by<  
+               hashed_unique< tag<block_id>, member< fork_item, block_id_type, &fork_item::id> >,
+               ordered_non_unique< tag<block_num>, member<fork_item,uint32_t,&fork_item::num> >
+            >
+         > fork_multi_index_type;
+
       private:
-         unordered_map< block_id_type, shared_ptr<fork_item> > _by_id;
-         vector< vector<shared_ptr<fork_item>> >               _by_num;
+         fork_multi_index_type _index;
          shared_ptr<fork_item>                                 _head;
    };
 } } // bts::chain 
