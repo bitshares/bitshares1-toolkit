81867779a0907e0dac7be48ec33938a2318aeea8
Daniel Larimer <dlarimer@invictus-innovations.com>
2015-03-18 18:59:37 -0400

    fix build

--- libraries/chain/delegate_object.cpp
+++ libraries/chain/delegate_object.cpp
@@ -4,7 +4,7 @@ using namespace bts::chain;
 
 const price_feed* delegate_feeds_object::get_feed(asset_id_type base, asset_id_type quote) const
 {
-   price_feed* result = nullptr;
+   const price_feed* result = nullptr;
    price_feed dummy;
    dummy.call_limit = price({asset(1, base), asset(1, quote)});
    auto itr = feeds.find(dummy);
