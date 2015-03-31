1ad6aa21d14a554abf6968f726ff2712b1dc8eda
Daniel Larimer <dan@bitshares.org>
2015-03-31 16:20:35 -0400

    progress toward stubbing out a reference wallet api

--- programs/cli_wallet/main.cpp
+++ programs/cli_wallet/main.cpp
@@ -2,6 +2,7 @@
 #include <fc/io/json.hpp>
 #include <fc/network/http/websocket.hpp>
 #include <fc/rpc/websocket_api.hpp>
+#include <fc/io/stdio.hpp>
 #include <iostream>
 
 
@@ -18,6 +19,55 @@ struct wallet_data
    string                    ws_password;
 };
 FC_REFLECT( wallet_data, (accounts)(keys)(ws_server)(ws_user)(ws_password) );
+
+
+/**
+ *  This wallet assumes nothing about where the database server is
+ *  located and performs minimal caching.  This API could be provided
+ *  locally to be used by a web interface.
+ */
+class wallet_api
+{
+   public:
+      wallet_api( fc::api<login_api> rapi )
+      :_remote_api(rapi)
+      {
+         _remote_db  = _remote_api->database();
+         _remote_net = _remote_api->network();
+      }
+      string  suggest_brain_key()const
+      {
+
+      }
+
+      signed_transaction create_account( string brain_key,
+                                         string account_name,
+                                         string pay_from_account )
+      {
+
+        return signed_transaction();
+      }
+
+      signed_transaction transfer( string from, 
+                                   string to, 
+                                   uint64_t amount,
+                                   string asset,
+                                   string memo,
+                                   bool broadcast = false )
+      {
+        return signed_transaction();
+      }
+
+      fc::api<login_api>      _remote_api;
+      fc::api<database_api>   _remote_db;
+      fc::api<network_api>    _remote_net;
+};
+
+FC_API( wallet_api, 
+        (suggest_brain_key)
+        (create_account) 
+        (transfer)
+       )
 
 int main( int argc, char** argv )
 {
@@ -36,9 +86,50 @@ int main( int argc, char** argv )
       auto remote_api = apic->get_remote_api< login_api >();
       FC_ASSERT( remote_api->login( wallet.ws_user, wallet.ws_password ) );
 
-      auto db  = remote_api->database();
-      auto net = remote_api->network();
+      auto wapi = std::make_shared<wallet_api>(remote_api);
       
+      std::string line;
+      while( true )
+      {
+         std::cout << ">>> ";
+         fc::getline( fc::cin, line );
+         std::cout << line <<"\n";
+         try {
+            if( line == "help" )
+            {
+               std::cout << "Commands:\n";
+               std::cout << "\tquit\n";
+               std::cout << "\tcreate account\n";
+               std::cout << "\tdisplay account\n";
+               std::cout << "\ttransfer\n";
+            }
+            else if( line == "quit" )
+            {
+               fc::json::save_to_file( fc::variant( wallet ), wallet_file );
+               return 0;
+            }
+            else if( line == "create account" )
+            {
+
+            }
+            else if( line == "display account" )
+            {
+
+            }
+            else if( line == "transfer" )
+            {
+
+            }
+            else 
+            {
+               std::cout << "Unknown Command: '" << line <<"'\n\n";
+            }
+         } 
+         catch ( const fc::exception& e )
+         {
+            std::cout << e.to_detail_string() << "\n";
+         }
+      }
    } 
    catch ( const fc::exception& e )
    {
