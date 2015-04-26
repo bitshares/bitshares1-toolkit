f60483dd36c770b3b945c677fe107f7e2d894541
theoretical <theoreticalbts@users.noreply.github.com>
2015-04-26 08:40:49 -0400

    ahplugin:  Implement key_account_index

--- libraries/plugins/account_history/account_history_plugin.cpp
+++ libraries/plugins/account_history/account_history_plugin.cpp
@@ -1,5 +1,12 @@
+
 #include <bts/account_history/account_history_plugin.hpp>
 
+#include <bts/chain/account_evaluator.hpp>
+#include <bts/chain/account_object.hpp>
+#include <bts/chain/config.hpp>
+#include <bts/chain/database.hpp>
+#include <bts/chain/evaluator.hpp>
+#include <bts/chain/key_object.hpp>
 #include <bts/chain/time.hpp>
 #include <bts/chain/operation_history_object.hpp>
 #include <bts/chain/account_object.hpp>
@@ -11,6 +18,50 @@ namespace bts { namespace account_history {
 namespace detail
 {
 
+class account_observer : public bts::chain::evaluation_observer
+{
+   public:
+      account_observer( account_history_plugin& plugin )
+          : _plugin( plugin )
+      {
+         _pre_account_keys.reserve( BTS_DEFAULT_MAX_AUTHORITY_MEMBERSHIP * 2 + 2 );
+      }
+      virtual ~account_observer();
+
+      virtual void pre_evaluate(
+          const transaction_evaluation_state& eval_state,
+          const operation& op,
+          bool apply,
+          generic_evaluator* ge ) override;
+
+      virtual void post_evaluate(
+          const transaction_evaluation_state& eval_state,
+          const operation& op,
+          bool apply,
+          generic_evaluator* ge ) override;
+
+      virtual void evaluation_failed(
+          const transaction_evaluation_state& eval_state,
+          const operation& op,
+          bool apply,
+          generic_evaluator* ge ) override;
+
+      /**
+       * Helper method to get the set of account keys
+       * that could be touched by the update operation.
+       *
+       * This observer's "diff" is get_updatable_account_keys() before
+       * and after the operation is applied.
+       */
+      void get_updatable_account_keys(
+          const account_update_operation& update_op,
+          flat_set< key_id_type > result
+          );
+
+      account_history_plugin& _plugin;
+      flat_set< key_id_type > _pre_account_keys;
+};
+
 class account_history_plugin_impl
 {
    public:
@@ -18,23 +69,28 @@ class account_history_plugin_impl
          account_history_plugin& _plugin,
          bts::chain::database& chain_db
          ) : _self( _plugin ),
+             _observer( _plugin ),
              _chain_db( chain_db )
          {}
       virtual ~account_history_plugin_impl();
+
+      flat_set<key_id_type> get_keys_for_account(
+          const account_id_type& account_id );
 
       /** this method is called as a callback after a block is applied
        * and will process/index all operations that were applied in the block.
        */
       void update_account_histories( const signed_block& b );
+      void index_account_keys( const account_id_type& account_id );
       // protected in abstract_plugin interface,
       //  we publish it here...if you can see this impl class,
       //  you know what you doing for great justice
       bts::chain::database& database() { return _chain_db; }
 
       account_history_plugin& _self;
+      account_observer _observer;
       bts::chain::database& _chain_db;
 };
-
 
 struct operation_get_impacted_accounts
 {
@@ -144,10 +200,209 @@ struct operation_get_impacted_accounts
    void operator()( const create_bond_offer_operation& o )const { }
 };
 
+account_observer::~account_observer()
+{
+    return;
+}
 
+void account_observer::get_updatable_account_keys(
+   const account_update_operation& update_op,
+   flat_set< key_id_type > result
+   )
+{
+   // add all keys which are in key fields touched by the update_op
+   //  to the given flat_set
+
+   const bts::chain::database& db = _plugin._my->database();
+   const account_object& acct = update_op.account(db);
+
+   if( update_op.voting_key.valid() )
+      result.insert( acct.voting_key );
+   if( update_op.memo_key.valid() )
+      result.insert( acct.memo_key );
+   if( update_op.owner.valid() )
+   {
+      for( const pair<object_id_type, weight_type>& item : acct.owner.auths )
+      {
+         if( item.first.type() == key_object_type )
+            result.insert( item.first );
+      }
+   }
+   if( update_op.active.valid() )
+   {
+      for( const pair<object_id_type, weight_type>& item : acct.active.auths )
+      {
+         if( item.first.type() == key_object_type )
+            result.insert( item.first );
+      }
+   }
+   return;
+}
+
+void account_observer::pre_evaluate(
+    const transaction_evaluation_state& eval_state,
+    const operation& op,
+    bool apply,
+    generic_evaluator* ge )
+{
+   FC_ASSERT( op.which() == operation::tag<account_update_operation>::value );
+
+   // if we only care about given accounts, then key -> account mapping
+   //   is not maintained
+   if( _plugin._config.accounts.size() > 0 )
+       return;
+
+   // is this a tx which affects a key?
+   // switch( op.which() )
+   // {
+   // see note in configure() why account_create_operation handling is unnecessary
+   //   case operation::tag<account_create_operation>::value:
+   //      const account_create_operation& create_op = op.get< account_create_operation >();
+   //      break;
+   //   case operation::tag<account_update_operation>::value:
+   //      const account_update_operation& update_op = op.get< account_update_operation >();
+   //      _pre_account_keys.clear();
+   //      get_updatable_account_keys( update_op, _pre_account_keys );
+   //      break;
+   //   default:
+   //      FC_ASSERT( false, "account_observer got unexpected operation type" );
+   //}
+
+   const account_update_operation& update_op = op.get< account_update_operation >();
+   _pre_account_keys.clear();
+   get_updatable_account_keys( update_op, _pre_account_keys );
+
+   return;
+}
+
+void account_observer::post_evaluate(
+    const transaction_evaluation_state& eval_state,
+    const operation& op,
+    bool apply,
+    generic_evaluator* ge
+    )
+{
+   FC_ASSERT( op.which() == operation::tag<account_update_operation>::value );
+
+   bts::chain::database& db = _plugin._my->database();
+
+   // if we only care about given accounts, then key -> account mapping
+   //   is not maintained
+   if( _plugin._config.accounts.size() > 0 )
+       return;
+
+   // wild back-of-envelope heuristic:  most account update ops
+   //   don't add more than two keys
+   flat_set<key_id_type> post_account_keys;
+   post_account_keys.reserve( _pre_account_keys.size() + 2 );
+
+   vector<key_id_type> removed_account_keys;
+   removed_account_keys.reserve( _pre_account_keys.size() );
+
+   const account_update_operation& update_op = op.get< account_update_operation >();
+   get_updatable_account_keys( update_op, post_account_keys );
+   std::set_difference(
+      _pre_account_keys.begin(), _pre_account_keys.end(),
+      post_account_keys.begin(), post_account_keys.end(),
+      removed_account_keys.begin()
+      );
+   for( const key_id_type& key_id : removed_account_keys )
+   {
+      auto& index = db.get_index_type<key_account_index>().indices().get<by_key>();
+      auto it = index.find( key_id(db).key_address() );
+      FC_ASSERT( it != index.end() );
+
+      db.modify<key_account_object>( *it, [&]( key_account_object& ka )
+      {
+         ka.account_ids.erase( update_op.account );
+      });
+   }
+   return;
+}
+
+void account_observer::evaluation_failed(
+    const transaction_evaluation_state& eval_state,
+    const operation& op,
+    bool apply,
+    generic_evaluator* ge
+    )
+{
+   // if we only care about given accounts, then key -> account mapping
+   //   is not maintained
+   if( _plugin._config.accounts.size() > 0 )
+       return;
+
+   // cleaning up here is not strictly necessary, but good "hygiene"
+   _pre_account_keys.clear();
+   return;
+}
 
 account_history_plugin_impl::~account_history_plugin_impl()
 {
+   return;
+}
+
+flat_set<key_id_type> account_history_plugin_impl::get_keys_for_account( const account_id_type& account_id )
+{
+   const bts::chain::database& db = database();
+   const account_object& acct = account_id(db);
+
+   const flat_map<object_id_type, weight_type>& owner_auths =
+       acct.owner.auths;
+   const flat_map<object_id_type, weight_type>& active_auths =
+       acct.active.auths;
+
+   flat_set<key_id_type> key_id_set;
+   key_id_set.reserve(owner_auths.size() + active_auths.size() + 2);
+
+   key_id_set.insert(acct.memo_key);
+   key_id_set.insert(acct.voting_key);
+
+   // we don't use get_keys() here to avoid an intermediate copy operation
+   for( const pair<object_id_type, weight_type>& item : active_auths )
+   {
+      if( item.first.type() == key_object_type )
+         key_id_set.insert( item.first );
+   }
+
+   for( const pair<object_id_type, weight_type>& item : owner_auths )
+   {
+      if( item.first.type() == key_object_type )
+         key_id_set.insert( item.first );
+   }
+
+   return key_id_set;
+}
+
+void account_history_plugin_impl::index_account_keys( const account_id_type& account_id )
+{
+   // for each key in account authority... get address, modify(..)
+   bts::chain::database& db = database();
+
+   flat_set<key_id_type> key_id_set = get_keys_for_account( account_id );
+
+   flat_set<address> address_set;
+
+   //
+   // we pass the addresses through another flat_set because the
+   //    blockchain doesn't force de-duplication of addresses
+   //    (multiple key_id's might refer to the same address)
+   //
+   address_set.reserve( key_id_set.size() );
+
+   for( const key_id_type& key_id : key_id_set )
+      address_set.insert( key_id(db).key_address() );
+
+   // add mappings to the given index
+   for( const address& addr : address_set )
+   {
+       db.create<key_account_object>( [&]( key_account_object& ka )
+       {
+          ka.key = addr;
+          ka.account_ids.insert( account_id );
+       });
+   }
+
    return;
 }
 
@@ -172,6 +427,8 @@ void account_history_plugin_impl::update_account_histories( const signed_block& 
       {
          for( auto& account_id : impacted )
          {
+            index_account_keys( account_id );
+
             // add history
             const auto& stats_obj = account_id(db).statistics(db);
             const auto& ath = db.create<account_transaction_history_object>( [&]( account_transaction_history_object& obj ){
@@ -189,6 +446,8 @@ void account_history_plugin_impl::update_account_histories( const signed_block& 
          {
             if( impacted.find( account_id ) != impacted.end() )
             {
+               index_account_keys( account_id );
+
                // add history
                const auto& stats_obj = account_id(db).statistics(db);
                const auto& ath = db.create<account_transaction_history_object>( [&]( account_transaction_history_object& obj ){
@@ -222,6 +481,16 @@ void account_history_plugin::configure(const account_history_plugin::plugin_conf
    database().applied_block.connect( [&]( const signed_block& b){ _my->update_account_histories(b); } );
    database().add_index< primary_index< simple_index< operation_history_object > > >();
    database().add_index< primary_index< simple_index< account_transaction_history_object > > >();
+   database().add_index< primary_index< key_account_index >>();
+   // account_create_operation is actually unnecessary
+   //    because all the necessary keys will be touched when
+   //    update_account_histories() is called, unless they are removed
+   //
+   // and the only way they can be removed is by account_update_operation
+   //    which will mark the removed keys for updating
+   //
+   // database().register_evaluation_observer<account_create_evaluator>( _my->_observer );
+   database().register_evaluation_observer< bts::chain::account_update_evaluator >( _my->_observer );
 }
 
 } }
--- libraries/plugins/account_history/include/bts/account_history/account_history_plugin.hpp
+++ libraries/plugins/account_history/include/bts/account_history/account_history_plugin.hpp
@@ -8,6 +8,30 @@
 namespace bts { namespace account_history {
 using namespace chain;
 
+class key_account_object : public abstract_object<key_account_object>
+{
+   public:
+      static const uint8_t space_id = 5;   // namespace for this plugin object type
+      static const uint8_t type_id  = 0;
+
+      key_account_object() {}
+      key_account_object( const address& a ) : key(a) {}
+
+      address                   key;
+      flat_set<account_id_type> account_ids;
+};
+
+struct by_key{};
+typedef multi_index_container<
+   key_account_object,
+   indexed_by<
+      hashed_unique< tag<by_id>, member< object, object_id_type, &object::id > >,
+      ordered_unique< tag<by_key>, member< key_account_object, address, &key_account_object::key > >
+   >
+> key_account_object_multi_index_type;
+
+typedef generic_index<key_account_object, key_account_object_multi_index_type> key_account_index;
+
 namespace detail
 {
     class account_history_plugin_impl;
@@ -38,6 +62,12 @@ class account_history_plugin : public bts::app::plugin<account_history_plugin>
 };
 
 } } //bts::account_history
+
+FC_REFLECT_DERIVED( bts::account_history::key_account_object,
+            (bts::db::object),
+            (key)
+            (account_ids)
+          )
 
 FC_REFLECT( bts::account_history::account_history_plugin::plugin_config,
             (accounts)
